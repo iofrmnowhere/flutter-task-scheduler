ca57f06a3c5f673f4468b7accb72b100c2a75b22
iofrmnowhere <gioferreras@gmail.com>
2025-11-05 21:14:20 +0800

    added add task widget

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,15 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   List<Widget> _widgetList = [];
 
+  void _showForm() async{
+    final result = await showDialog<Map<String, dynamic>>(
+        context: context,
+        builder: (BuildContext context){
+          return const TaskForm();
+        }
+    );
+  }
+
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -111,20 +120,182 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
+
+
       body: ListView(
           children: <Widget>[
             ..._widgetList,
           ],
         ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _showForm,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        child: const Icon(Icons.add),
 
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
 
     );
   }
+}
+
+class TaskForm extends StatefulWidget{
+  const TaskForm({super.key});
+  @override
+  State<TaskForm> createState() => _TaskFormState();
+}
+
+class _TaskFormState extends State<TaskForm>{
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _txtCtrl = TextEditingController();
+  DateTime? _slctdate;
+  TimeOfDay? _slcttime;
+  //pang format
+  String get _fmtDate{
+    if (_slctdate == null){
+      return "";
+    }
+    return "${_slctdate!.day}/${_slctdate!.month}/${_slctdate}";
+  }
+  String get _fmtTime{
+    if(_slcttime == null){
+      return '';
+    }
+    return _slcttime!.format(context);
+  }
+  //open pickers
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: _slctdate ?? now,
+        firstDate: now,
+        lastDate: DateTime(now.year + 5)
+    );
+    if (pickedDate != null) {
+      setState(() {
+        _slctdate = pickedDate;
+      });
+    }
+  }
+    void _presentTimePicker() async{
+      final now = TimeOfDay.now();
+      final pickedTime = await showTimePicker(
+          context: context,
+          initialTime: _slcttime ?? now
+      );
+      if (pickedTime != null){
+        setState(() {
+          _slcttime = pickedTime;
+        });
+      }
+    }
+
+    @override
+    void dispose(){
+      _txtCtrl.dispose();
+      super.dispose();
+    }
+
+    //validation and form submission
+    void _submitData(){
+      if(_formKey.currentState!.validate()){
+        final Map<String, dynamic> taskData = {
+          "name": _txtCtrl.text,
+          "date":_fmtDate,
+          "rawDate":_slctdate,
+          "time":_fmtTime,
+          "rawTime":_slcttime
+        };
+        Navigator.of(context).pop(taskData);
+      }
+    }
+    Widget build(BuildContext context){
+      return AlertDialog(
+        title: const Text("Enter a Task"),
+        content: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  //task name
+                  TextFormField(
+                    controller: _txtCtrl,
+                    decoration: const InputDecoration(
+                      labelText:"Task",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.label_outline)
+                    ),
+                    validator: (value){
+                      if(value == null || value.isEmpty){
+                        return "Please enter a Task";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  //date
+                  TextFormField(
+                    readOnly: true,
+                    controller:TextEditingController(text: _fmtDate),
+                    onTap: _presentDatePicker,
+                    decoration: InputDecoration(
+                      labelText: "Due Date",
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.calendar_today),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: _presentDatePicker,
+                      )
+                    ),
+                    validator: (value){
+                      if (_slctdate == null){
+                        return "Please Enter Due Date.";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  //time
+                  TextFormField(
+                    readOnly: true,
+                    controller: TextEditingController(text: _fmtTime),
+                    onTap: _presentTimePicker,
+                    decoration: InputDecoration(
+                      labelText: "Due Time",
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(Icons.access_time),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: _presentTimePicker,
+                      ),
+                    ),
+                    validator: (value){
+                      if(_slcttime == null){
+                        return "Please Enter Due Time";
+                      }
+                      return null;
+                    }
+                  ),
+                ],//parent ng mga forms
+              )
+          ),
+        ),
+        //buttons
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Cancel'),
+            onPressed: (){
+              Navigator.of(context).pop(null);
+            },
+          ),
+          ElevatedButton(
+              onPressed: _submitData,
+              child: const Text("Save"),
+          ),
+        ],
+      );
+    }
 }
